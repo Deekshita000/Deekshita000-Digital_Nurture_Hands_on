656acab1a62a0f75904427ceda9ffaa001f18209
Deekshita <dwarapureddydeekshita@gmail.com>
2026-07-24 10:09:42 +0530

    Added Module7

---
+++ Module7 - Spring REST/spring-learn/spring-learn/src/main/java/com/cognizant/spring_learn/Controller/AuthenticationController.java
@@ -0,0 +1,58 @@
+package com.cognizant.spring_learn.controller;
+
+import io.jsonwebtoken.JwtBuilder;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Base64;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class AuthenticationController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(AuthenticationController.class);
+    public static final String SECRET_KEY = "secretkeysecretkeysecretkeysecretkey";
+
+    @GetMapping("/authenticate")
+    public Map<String, String> authenticate(@RequestHeader("Authorization") String authHeader) {
+        LOGGER.info("START authenticate");
+
+        String user = getUser(authHeader);
+        String token = generateJwt(user);
+
+        Map<String, String> map = new HashMap<>();
+        map.put("token", token);
+
+        LOGGER.info("END authenticate");
+        return map;
+    }
+
+    private String getUser(String authHeader) {
+        String encodedCredentials = authHeader.substring(6);
+        byte[] decodedBytes = Base64.getDecoder().decode(encodedCredentials);
+        String credentials = new String(decodedBytes, StandardCharsets.UTF_8);
+        return credentials.split(":")[0];
+    }
+
+    private String generateJwt(String user) {
+        Key key = Keys.hmacShaKeyFor(SECRET_KEY.getBytes(StandardCharsets.UTF_8));
+
+        JwtBuilder builder = Jwts.builder()
+                .setSubject(user)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 1200000))
+                .signWith(key, SignatureAlgorithm.HS256);
+
+        return builder.compact();
+    }
+}
